f2888bd638d4252dda586a9a52bf32dc44571e44
Fred <apocalipsis201@gmail.com>
2016-02-15 14:16:52 -0600

    agregando pantalla de menu y modulo

--- moneycash/urls.py
+++ moneycash/urls.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
-from .views import HomePage
+from .views import *
 
 urlpatterns = [
     url(r'^$', HomePage.as_view(), name='moneycash'),
+    url(r'^modules/$', ModulesPage.as_view(), name='modules'),
 ]
--- moneycash/views.py
+++ moneycash/views.py
@@ -3,3 +3,6 @@ from django.views.generic.base import TemplateView
 
 class HomePage(TemplateView):
     template_name = "moneycash/index.html"
+
+class ModulesPage(TemplateView):
+    template_name = "moneycash/modules.html"

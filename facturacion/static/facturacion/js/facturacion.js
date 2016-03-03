a3ad719afe2181c720e63c94486dbd7ecce695da
xangmuve <cesarabel@johnmay.com.ni>
2016-03-03 16:07:21 -0600

    autocomplete ajax

--- facturacion/static/facturacion/js/facturacion.js
+++ facturacion/static/facturacion/js/facturacion.js
@@ -1,3 +1,22 @@
-$(function() {
-    console.log("hi,,");
+$(document).ready(function(){
+
+
+    $( document ).ready(function() {
+        $('#id_name,#id_identificacion').each(function()
+                                                                {
+                                                                    $(this).autocomplete({
+                                                                        minLength: 2,
+                                                                        source: "{% url 'autocomplete_cliente' %}",
+                                                                        select: function( event, ui ) {
+                                                                            $('#id_code').val(ui.item.obj.code);
+                                                                            $('#id_name').val(ui.item.obj.name);
+                                                                            $('#id_identificacion').val(ui.item.obj.identificacion);
+                                                                            $('#id_telefono').val(ui.item.obj.telefono);
+                                                                            $('#id_email').val(ui.item.obj.email);
+                                                                            $('#id_direccion').val(ui.item.obj.direccion);
+                                                                        }
+                                                                    });
+                                                                }
+                                                               );
+    });
 });

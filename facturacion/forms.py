2df82fb24ba446c351319c30b1400fda79a63794
fred <apocalipsis201@gmail.com>
2016-03-07 22:33:37 -0600

    calculo de factura

--- facturacion/forms.py
+++ facturacion/forms.py
@@ -37,40 +37,59 @@ class DetalleForm(forms.ModelForm):
 
 class FacturaForm(forms.ModelForm):
 
-    numero = forms.CharField(label='numero de factura', max_length=14,
+    numero = forms.CharField(label='Numero de factura', max_length=14,
         widget=forms.TextInput(attrs={'readonly': 'true'}))
 
-    fecha = forms.CharField(label='fecha', max_length=50,
+    fecha = forms.CharField(label='Fecha', max_length=50,
         widget=forms.TextInput(attrs={'readonly': 'true'}))
 
-    code = forms.CharField(label='codigo del cliente', max_length=30,
+    code = forms.CharField(label='Codigo del cliente', max_length=30,
         widget=forms.TextInput(attrs={'readonly': 'true'}))
 
-    name = forms.CharField(label='nombre del cliente', max_length=125,
+    name = forms.CharField(label='Nombre del cliente', max_length=125,
         widget=forms.TextInput(attrs={'class': 'datos_cliente',
+            'autocomplete': 'off', 'autofocus':'true',
+            'required':'true'}))
+
+    identificacion = forms.CharField(label='Identificacion del cliente',
+        max_length=14, widget=forms.TextInput(attrs={'class': 'datos_cliente',
+            'autocomplete': 'off', 'required':'true'}))
+
+    telefono = forms.CharField(label='Telefono del cliente',
+        max_length=25, widget=forms.TextInput(attrs={'class': 'datos_cliente',
             'autocomplete': 'off'}))
 
-    identificacion = forms.CharField(label='identificacion del cliente',
-        max_length=14, widget=forms.TextInput(attrs={'class': 'datos_cliente',
+    email = forms.CharField(label='Email del cliente',
+        max_length=80, widget=forms.EmailInput(attrs={'class': 'datos_cliente',
+            'autocomplete': 'off'}))
+
+    direccion = forms.CharField(label='Direcion del cliente',
+        max_length=255, widget=forms.Textarea(attrs={'class': 'datos_cliente',
             'autocomplete': 'off'}))
 
     subtotal = forms.CharField(label='subtotal', max_length=14,
-        widget=forms.TextInput(attrs={'readonly': 'true'}))
+        widget=forms.TextInput(attrs={'readonly': 'true',
+        'value':'0.00'}))
 
     descuento = forms.CharField(label='descuento', max_length=14,
-        widget=forms.TextInput(attrs={'readonly': 'true'}))
+        widget=forms.TextInput(attrs={'readonly': 'true',
+        'value':'0.00'}))
 
     iva = forms.CharField(label='iva', max_length=14,
-        widget=forms.TextInput(attrs={'readonly': 'true'}))
+        widget=forms.TextInput(attrs={'readonly': 'true',
+        'value':'0.00'}))
 
     ir = forms.CharField(label='retencion del ir', max_length=14,
-        widget=forms.TextInput(attrs={'readonly': 'true'}))
+        widget=forms.TextInput(attrs={'readonly': 'true',
+        'value':'0.00'}))
 
     al = forms.CharField(label='retencion de la alcaldia', max_length=14,
-        widget=forms.TextInput(attrs={'readonly': 'true'}))
+        widget=forms.TextInput(attrs={'readonly': 'true',
+        'value':'0.00'}))
 
     total = forms.CharField(label='total', max_length=14,
-        widget=forms.TextInput(attrs={'readonly': 'true'}))
+        widget=forms.TextInput(attrs={'readonly': 'true',
+         'value':'0.00'}))
 
     class Meta:
         model = Factura
--- facturacion/static/facturacion/js/facturacion.js
+++ facturacion/static/facturacion/js/facturacion.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 try {
+    var total = 0;
     $('form').on('submit', function(e) {
         return false;
     });
@@ -9,9 +10,41 @@ try {
     $('#factura_form').on('keyup', '.producto_code', function() {
         complete_producto($(this));
     });
+    $('.box_cliente').on('mouseover', 'h2 .borrar', function() {
+        $('.datos_cliente')
+            .css({'text-decoration': 'line-through', 'color': 'red'});
+    }).on('mouseout', 'h2 .borrar', function() {
+        $('.datos_cliente')
+            .css({'text-decoration': 'none', 'color': 'black'});
+    }).on('click', 'h2 .borrar', function(e) {
+        e.preventDefault;
+        borrar('.datos_cliente');
+        readonly('.datos_cliente', false);
+    });
+    $('.grp-table').on('change', '.grp-dynamic-form .cantidad .producto_cantidad'
+        , function(e) {
+            $(this).parents('.grp-tr')
+                .find('.producto_total')
+                .val(get_total($(this).val(), $(this).parents('.grp-tr')
+                        .find('.producto_precio').val()));
+            $('.producto_total').trigger('change');
+    });
+    $('.grp-table').on('change', '.grp-dynamic-form .total .producto_total'
+        , function(e) {
+        $('#id_subtotal').val(parseFloat($('#id_subtotal').val())+parseFloat($(this).val()));
+    });
     function get_total(cantidad, precio) {
         return parseFloat(cantidad) * parseFloat(precio);
     }
+    /*function get_sub_total(clase) {
+        var tmp = 0;
+        $(clase).each(function(key, value) {
+            tmp += parseFloat(value);
+            if(key == ($(clase).length -1)) {
+                return tmp;
+            }
+        });
+    }*/
     function complete_cliente(self) {
         if($.trim($(self).val()) != '') {
             $(self).autocomplete({
@@ -26,6 +59,7 @@ try {
                     $('#id_email').val(ui.item.obj.email);
                     $('#id_direccion').val(ui.item.obj.direccion);
                     $('.box_cliente .grp-collapse-handler').empty().append("Datos del Cliente<span class='borrar'>X</span>");
+                    readonly('.datos_cliente', true);
                 }
             });
         }
@@ -42,9 +76,16 @@ try {
                     $(self).parents('.grp-tr').find('.producto_descuento').val(0.0);
                     $(self).parents('.grp-tr').find('.producto_iva').val(0.0);
                     $(self).parents('.grp-tr').find('.producto_total').val(get_total(1, ui.item.obj.precio));
+                    $('.producto_total').trigger('change');
                 }
             });
         }
+    }
+    function borrar(clase) {
+        $(clase).val('');
+    }
+    function readonly(clase, option) {
+        $(clase).prop('readonly', option);
     }
 } catch (err) {
     console.log(err);

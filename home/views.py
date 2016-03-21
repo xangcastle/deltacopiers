2b344e8cf868a50fe3a5439761a27487187b7043
xangmuve <cesarabel@johnmay.com.ni>
2016-03-21 16:44:21 -0600

    encuentra banpro

--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home/index.html"
+
+
+class EncuentraBanproView(TemplateView):
+    template_name = "home/encuentrabanpro.html"

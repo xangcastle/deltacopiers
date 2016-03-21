8233afb84c504695697c308404ea4df1efb8f688
xangmuve <cesarabel@johnmay.com.ni>
2016-03-21 15:38:56 -0600

    prueba puntos encuentra banpro

--- deltacopiers/settings.py
+++ deltacopiers/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    #'registration',
+    'banpro',
     'home',
     'moneycash',
 ]

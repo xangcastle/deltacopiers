347ee27af4e108d6aaf2fff2441ae66edbe3a871
xangmuve <cesarabel@johnmay.com.ni>
2016-03-21 15:40:58 -0600

    prueba puntos encuentra banpro

--- banpro/models.py
+++ banpro/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 
 class Puntos(models.Model):
-    tipo = models.CharField(max_lenght=125, null=True, blank=True)
+    tipo = models.CharField(max_length=125, null=True, blank=True)
     lat = models.FloatField(null=True, blank=True)
     lon = models.FloatField(null=True, blank=True)
-    direccion = models.TextField(max_lenght=255, null=True, blank=True)
-    nombre = models.CharField(max_lenght=125, null=True, blank=True)
-    horario = models.CharField(max_lenght=125, null=True, blank=True)
+    direccion = models.TextField(max_length=255, null=True, blank=True)
+    nombre = models.CharField(max_length=125, null=True, blank=True)
+    horario = models.CharField(max_length=125, null=True, blank=True)
 
     def __unicode__(self):
         return self.nombre

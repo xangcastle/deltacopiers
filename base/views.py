a6d88114d964850a4be22fcaef6e556bcb64c597
xangmuve <cesarabel@johnmay.com.ni>
2016-07-07 21:06:36 -0600

    mejora en busqueda de productos

--- base/views.py
+++ base/views.py
@@ -2,35 +2,32 @@ from django.http.response import HttpResponse
 from django.forms.models import model_to_dict
 from django.db.models import Q
 import json
+import operator
+
+
+def find_entidad(sentence):
+    words = sentence.split(" ")
+    predicates = []
+    for word in words:
+        predicates.append(('name__icontains', word))
+    return [Q(x) for x in predicates]
 
 
 def autocomplete_entidad(instance, request):
+    data = []
     if request.is_ajax:
         model = type(instance)
         result = []
         term = request.GET.get('term', None)
-        code = request.GET.get('code', None)
         if term:
-            qs = model.objects.filter(
-                Q(code__istartswith=term) |
-                Q(name__icontains=term)
-                )
+            qs = model.objects.filter(reduce(operator.and_, find_entidad(term)))
             for obj in qs:
                 obj_json = {}
                 obj_json['label'] = obj.name
                 obj_json['value'] = obj.name
                 obj_json['obj'] = model_to_dict(obj)
                 result.append(obj_json)
-        if code:
-            obj = model.objects.get(code=code)
-            obj_json = {}
-            obj_json['label'] = obj.name
-            obj_json['value'] = obj.name
-            obj_json['obj'] = model_to_dict(obj)
-            result.append(obj_json)
         data = json.dumps(result)
-    else:
-        data = 'fail'
     return HttpResponse(data, content_type='application/json')
 
 

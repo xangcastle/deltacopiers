f5363e4875e92339cf63167b3147aa471a06c17c
xangmuve <cesarabel@johnmay.com.ni>
2016-07-07 19:28:24 -0600

    action factor

--- moneycash/admin.py
+++ moneycash/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 from base.admin import entidad_admin
-
+from django import forms
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 
 class cliente_admin(entidad_admin):
     list_display = ('code', 'name', 'ident', 'phone', 'email', 'address')
@@ -14,6 +17,39 @@ class producto_admin(entidad_admin):
     list_display = ('code', 'name', 'no_part', 'price', 'cost',
     'existencia_total')
     search_fields = ('code', 'name', 'no_part')
+
+    class factorForm(forms.Form):
+        _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+        factor = forms.FloatField(required=True,label= "Factor a Aplicar", widget=forms.NumberInput(attrs={'placeholder': 'Factor a Aplicar'}))
+
+
+    actions = ['aplicar_factor']
+
+    def aplicar_factor(self, request, queryset):
+        form = None
+        if 'apply' in request.POST:
+            form = self.factorForm(request.POST)
+            if form.is_valid():
+                factor = float(form.cleaned_data['factor'])
+                for q in queryset:
+                    q.price = q.cost * factor
+                    q.save()
+                return HttpResponseRedirect(
+                    "/admin/moneycash/producto")
+
+        if not form:
+            form = self.factorForm(
+                initial={
+                    '_selected_action': request.POST.getlist(
+                        admin.ACTION_CHECKBOX_NAME)})
+        data = {'queryset': queryset, 'form': form,
+                'header_tittle': 'Indique el Factor de Comercializacion que le aplicara a los productos selecionados.',
+                'explanation': 'El precio de los siguientes productos se vera afectado:',
+                'action': 'aplicar_factor'}
+        data.update(csrf(request))
+        return render_to_response('admin/moneycash/action.html', data)
+    aplicar_factor.short_description = "Aplicar factor a los productos selecionado/s"
+
 
 admin.site.register(Producto, producto_admin)
 

d7a02866b2502197263aff4e0fd84f4a0f952f73
xangmuve <cesarabel@johnmay.com.ni>
2016-07-13 19:43:29 -0600

    nuevos productos

--- tienda/views.py
+++ tienda/views.py
@@ -5,9 +5,14 @@ from moneycash.models import *
 class TiendaPageView(TemplateView):
     template_name = "tienda/index.html"
     def get_context_data(self, **kwargs):
+        ps = Producto.objects.all()
         context = super(TiendaPageView, self).get_context_data(**kwargs)
         context['carrusel'] = []
-        for p in Producto.objects.all()[:5]:
+        for p in ps[:5]:
             if p.imagen:
                 context['carrusel'].append(p)
+        context['featured'] = []
+        for p in ps[5:15]:
+            if p.imagen:
+                context['featured'].append(p)
         return context

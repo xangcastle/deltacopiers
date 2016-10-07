f0043f50a24c8e03123fb91b926cec71a2125f77
xangmuve <cesarabel@johnmay.com.ni>
2016-10-06 21:32:42 -0600

    filtro por estado

--- importaciones/admin.py
+++ importaciones/admin.py
@@ -6,5 +6,6 @@ class importacion_admin(admin.ModelAdmin):
     date_hierarchy = 'fecha'
     list_display = ('nombre', 'estado')
     change_form_template = "admin/moneycash/modelo.html"
+    list_filter = ('estado', )
 
 admin.site.register(Importacion, importacion_admin)

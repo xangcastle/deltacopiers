0414e02d09a6b09ff73094f464f6128b1a1a6b5a
xangmuve <cesarabel@johnmay.com.ni>
2016-10-10 23:06:35 -0600

    mejoras sistema de importaciones

--- importaciones/admin.py
+++ importaciones/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import *
 
 
+class item_admin(admin.TabularInline):
+    model = Item
+    extra = 0
+
 class importacion_admin(admin.ModelAdmin):
     date_hierarchy = 'fecha'
     list_display = ('nombre', 'estado')
-    change_form_template = "admin/moneycash/modelo.html"
+    change_form_template = "admin/importacion.html"
     list_filter = ('estado', )
+    inlines = [item_admin,]
+    fields = (('nombre', 'fecha'), 'blog', ('proforma_proveedor', 'proforma'),
+        ('flete', 'aduanas'), ('divisa', 'banco'))
 
 admin.site.register(Importacion, importacion_admin)
--- importaciones/models.py
+++ importaciones/models.py
@@ -4,9 +4,25 @@ from django.db import models
 
 
 class Importacion(models.Model):
+    change_form_template = "/admin/importacion.html"
     fecha = models.DateField(null=True)
     nombre = models.CharField(max_length=165)
     blog = models.TextField(max_length=99999)
     proforma = models.FileField(null=True, blank=True)
     proforma_proveedor = models.FileField(null=True, blank=True)
     estado = models.CharField(max_length=100, null=True, blank=True)
+    flete = models.FloatField(null=True, default=0.0)
+    aduanas = models.FloatField(null=True, default=0.0)
+    divisa = models.FloatField(null=True, default=0.0)
+    banco = models.FloatField(null=True, default=0.0)
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Item(models.Model):
+    orden = models.ForeignKey(Importacion)
+    cantidad = models.FloatField()
+    descripcion = models.CharField(max_length=300)
+    precio = models.FloatField()
+    anexo = models.FileField(null=True, blank=True)

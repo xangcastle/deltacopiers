b308d8328e81f9821c7318516c036b5c0e89f075
xangmuve <cesarabel@johnmay.com.ni>
2017-01-18 07:49:38 -0600

    pdf

--- lacteos/urls.py
+++ lacteos/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from .views import *
 
 urlpatterns = patterns('lacteos.views',
-    url(r'^print/recibos/(?P<id_recoleccion>.*)/$', imprimir_recoleccion.as_view(),name='imprimir_recoleccion'),
+    url(r'^print/recibos/(?P<id_recoleccion>.*)/$', 'imprimir_recoleccion',name='imprimir_recoleccion'),
     url(r'^print/retenciones/(?P<id_recoleccion>.*)/$','imprimir_retencion',name='imprimir_retencion'),
     url(r'^print/productores/(?P<id_linea>.*)/$','lista_productores',name='lista_productores'),
     url(r'^print/detalle_linea/(?P<id_recoleccion>.*)/$','detalle_linea',name='detalle_linea'),
